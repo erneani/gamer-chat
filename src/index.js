c35228664e72dd3c57e4e5b1158c299f9101a1e6
Vinícius Ernani de Carvalho <viniciusernanicarvalho@gmail.com>
2020-05-23 00:58:18 -0300

    Remove the app component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import App from "./App";
+import Login from "./pages/Login";
 import "./reset.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={Login} />
       </Switch>
     </Router>
   </React.StrictMode>,
