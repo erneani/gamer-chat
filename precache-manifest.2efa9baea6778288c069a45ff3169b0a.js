7d223d80f9673a359263412465974d70df2652b6
Vinícius Ernani de Carvalho <viniciusernanicarvalho@gmail.com>
2020-05-23 19:33:08 -0300

    Updates

---
+++ precache-manifest.2efa9baea6778288c069a45ff3169b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de0f866aacf2ab4290e8e78ad4671a3",
+    "url": "/gamer-chat/index.html"
+  },
+  {
+    "revision": "1caf3c164e64633385af",
+    "url": "/gamer-chat/static/css/main.eaa2d99c.chunk.css"
+  },
+  {
+    "revision": "9966250cf21b37790e4f",
+    "url": "/gamer-chat/static/js/2.d913dead.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gamer-chat/static/js/2.d913dead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1caf3c164e64633385af",
+    "url": "/gamer-chat/static/js/main.cfcb1c1f.chunk.js"
+  },
+  {
+    "revision": "c7089e7d32bb6d991167",
+    "url": "/gamer-chat/static/js/runtime-main.725aaec5.js"
+  }
+]);
